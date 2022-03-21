9a26228190a33eff92276949bd09b8444993ff8c
Andreasa Martin <andreasamartin@138-38-190-81.eduroam.bath.ac.uk>
2022-03-21 14:00:30 +0000

    working version before major refactoring for pipeline streamling, removing unnecessary activities

--- src/java/Tester.java
+++ src/java/Tester.java
@@ -1,18 +1,38 @@
+import java.io.File;
+import java.io.IOException;
 
-
-
+import org.apache.commons.io.FileUtils;
 
 public class Tester {
 	
-	
+	public Tester()
+	{
+		
+	}
 	
 	public static void main(String[] args) {
 		
 		System.out.println("hello");
+		String file1 = "/Users/andreasamartin/Documents/InstalExamples/rooms/rooms_revised_synthesizer1_6.ial";
+		String file2 = "/Users/andreasamartin/Documents/InstalExamples/rooms/rooms_revised_synthesizer2_7.ial";
+		//String file2 = "/Users/andreasamartin/Documents/InstalExamples/rooms/roomsInst.ial";
 		
-		JsonExtractor x =  new JsonExtractor();
-		System.out.println("////\\\\\n"+x.modifyFact("holdsat(capacityExceededViol(room1),rooms,0).","capacityExceededViol", "capacityExceededViol(room1)", 3, 1));
-		
+		//System.out.println("////\\\\\n"+x.modifyFact("holdsat(capacityExceededViol(room1),rooms,0).","capacityExceededViol", "capacityExceededViol(room1)", 3, 1));
+		Tester test = new Tester();
+//		.
+		try {
+			System.out.println("////\\\\\\\n"+test.compareFiles(file1,file2));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
+	private boolean compareFiles(String file1_path,String file2_path) throws IOException
+	{
+		boolean comp = FileUtils.contentEquals(new File(file1_path),new File(file2_path));
+		//FileUtils.co
+	
+		return comp;
+	}
 }
